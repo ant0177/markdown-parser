3827539fe2cfb9e02c9c505e1f9f45f6cd3500ed
ant0177 <ant017@ucsd.edu>
2022-05-30 21:22:43 -0700

    fix?

--- MarkDownParseTest.java
+++ MarkDownParseTest.java
@@ -1,68 +1,185 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
-import java.io.IOException;
+import java.util.*;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+
 
 public class MarkdownParseTest {
 
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
-        List<String> expect = List.of("https://something.com", "some-thing.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void addition() {
+        assertEquals(2, 1+1);
     }
 
     @Test
-    public void testFile2() throws IOException {
-        String contents= Files.readString(Path.of("./test-file2.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testParse1() throws IOException {
+        
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+
+        Path fileName = Path.of("./test1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test1.md"
     }
 
     @Test
-    public void testLinkAtBeginning() {
-        String contents= "[link title](a.com)";
-        List<String> expect = List.of("a.com");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testBreakFile1() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+
+        Path fileName = Path.of("./break-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/break-file.md"
     }
 
     @Test
-    public void testSpaceInURL() {
-        String contents = "[title](space in-url.com)";
-        List<String> expect = List.of();
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testBreakFile2() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+
+        Path fileName = Path.of("./break-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/break-file2.md"
     }
 
     @Test
-    public void testSpaceAfterParen() {
-        String contents = "[title]( space-in-url.com)";
-        List<String> expect = List.of("space-in-url.com");
-        assertEquals(expect, MarkdownParse.getLinks(contents));
+    public void testBreakFile3() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("someLink.com");
+
+        Path fileName = Path.of("./break-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/break-file3.md"
     }
 
     @Test
-    public void testSpaceBeforeParen() {
-        String contents = "[title]   (should-not-count.com)";
-        List<String> expect = List.of();
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testExtraText() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        expected.add("https://something.com");
+
+        Path fileName = Path.of("./test-file2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file2.md"
     }
 
     @Test
-    public void testNestedParens() throws IOException {
-        String contents = Files.readString(Path.of("test-parens-inside-link.md"));
-        List<String> expect = List.of("something.com()", "something.com((()))", "something.com", "boring.com");
-        assertEquals(expect, MarkdownParse.getLinks(contents));
+    public void testNoLinkExtraText() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+        
+        Path fileName = Path.of("./test-file3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file3.md"
+    }
+
+    public void testExtraSpace() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        expected.add("some-thing.html");
+
+        Path fileName = Path.of("./test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file.md"
+    }
+    
+    @Test
+    public void testFileBreak1() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+        
+        Path fileName = Path.of("./test-file-break1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file-break1.md"
     }
 
     @Test
-    public void testMissingCloseParen() throws IOException {
-        String contents = Files.readString(Path.of("test-missing-paren-plus-test-file2.md"));
-        List<String> expect = List.of("https://something.com", "some-page.html");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+    public void testFileBreak2() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+
+        Path fileName = Path.of("./test-file-break2.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file-break2.md"
     }
+
+    @Test
+    public void testFileBreak3() throws IOException {
+
+        ArrayList<String> expected = new ArrayList<>();
+
+        Path fileName = Path.of("./test-file-break3.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> actualLinks = MarkdownParse.getLinks(content);
+
+        assertEquals(expected, actualLinks);
+
+        // "/Users" + 
+        // "/ddavepersona/Documents/GitHub/markdown-parser-latest/" +
+        // "markdown-parser/test-file-break3.md"
+    }
+
     @Test
     public void testSnippet1() throws IOException { 
 
@@ -122,5 +239,8 @@ public class MarkdownParseTest {
         // "ddavepersona/Documents/GitHub/markdown-parser-latest/" +
         // "markdown-parser/snippet3.md"
     }
-    
 }
+
+// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar MarkdownParseTest.java MarkdownParse.java
+// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore MarkdownParseTest
+//changes
